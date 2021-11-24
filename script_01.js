d6c35488db2f9b58bdc5271017627f087c0f0719
Christian <chris.moeller-bosse@gmx.de>
2021-11-24 15:15:01 +0100

    Version 1

--- script_01.js
+++ script_01.js
@@ -12,17 +12,53 @@
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-output(getSentence(["Ich","bin","Max"],"S"));
-ausgabe(getSentence(["Bist","du","Max"],"Q"));
+output(getSentence(["Anführungszeichen", "nicht", "vergessen"], "E"));
+output(correctParas(["Bist", "du", "Max"], "12"));
+output(getSentence(["Bist", "du", "Max", "Mustermann", "der", "Dritte"], "e"));
 
-getSentence(arr,op){
-	
-	// ....
-	
+
+function getSentence(arr, op) {
+    let sentence = "";
+    const gap = " ";
+    for (let i = 0; i < arr.length; i++) {
+        sentence += arr[i]
+        if (i < arr.length - 1) {
+            sentence += gap;
+        }
+    }
+    sentence += choosePunctuation(op);
+    return sentence;
 }
 
+function choosePunctuation(op) {
+    switch (op) {
+        case "S":
+        case "s":
+            return ".";
+        case "Q":
+        case "q":
+            return "?";
+        case "E":
+        case "e":
+            return "!";
+        default:
+            return "stop";
+    }
+}
+
+function correctParas(arr, op) {
+    let punctuation = choosePunctuation(op);
+    if (arr.length == 0) {
+        return "Es wurden keine Wörter angegeben.";
+    }
+    else if (punctuation == "stop") {
+        return "Kein korrektes Satzzeichen.";
+    } else {
+        return "OK";
+    }
+}
 // Modul: Ausgabe in Konsole : Test
-// output("hi"); 
+//  output("hi"); 
 function output(outputStr) {
     console.log(outputStr);
 }
